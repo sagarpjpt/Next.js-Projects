97897dbc2093dee31200d3dfe4f3446b5efc6100
KattarCoder <sagarprjpt99@gmail.com>
2026-02-20 17:11:30 +0530

    css styling

--- nextjs-todo-prod/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold">Welcome to Todo App</h1>
-      <p className="mt-2 text-gray-600">Login to manage your todos.</p>
-    </main>
-  );
-}
